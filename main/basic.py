f8258e5f2865e689cc88417b498e9cf5f17f80b6
Erfan Hüseyini <56768093+erfgram@users.noreply.github.com>
2022-05-30 23:40:43 +0430

    good to go

--- main/basic.py
+++ main/basic.py
@@ -505,97 +505,6 @@ class BuiltInFunction(BaseFunction):
     return RTResult().success(Number(number))
   execute_input_int.arg_names = []
 
-  def execute_clear(self, exec_ctx):
-    os.system('cls' if os.name == 'nt' else 'cls') 
-    return RTResult().success(Number.null)
-  execute_clear.arg_names = []
-
-  def execute_is_number(self, exec_ctx):
-    is_number = isinstance(exec_ctx.symbol_table.get("value"), Number)
-    return RTResult().success(Number.true if is_number else Number.false)
-  execute_is_number.arg_names = ["value"]
-
-  def execute_is_string(self, exec_ctx):
-    is_number = isinstance(exec_ctx.symbol_table.get("value"), String)
-    return RTResult().success(Number.true if is_number else Number.false)
-  execute_is_string.arg_names = ["value"]
-
-  def execute_is_list(self, exec_ctx):
-    is_number = isinstance(exec_ctx.symbol_table.get("value"), List)
-    return RTResult().success(Number.true if is_number else Number.false)
-  execute_is_list.arg_names = ["value"]
-
-  def execute_is_function(self, exec_ctx):
-    is_number = isinstance(exec_ctx.symbol_table.get("value"), BaseFunction)
-    return RTResult().success(Number.true if is_number else Number.false)
-  execute_is_function.arg_names = ["value"]
-
-  def execute_append(self, exec_ctx):
-    list_ = exec_ctx.symbol_table.get("list")
-    value = exec_ctx.symbol_table.get("value")
-
-    if not isinstance(list_, List):
-      return RTResult().failure(RTError(
-        self.pos_start, self.pos_end,
-        "First argument must be list",
-        exec_ctx
-      ))
-
-    list_.elements.append(value)
-    return RTResult().success(Number.null)
-  execute_append.arg_names = ["list", "value"]
-
-  def execute_pop(self, exec_ctx):
-    list_ = exec_ctx.symbol_table.get("list")
-    index = exec_ctx.symbol_table.get("index")
-
-    if not isinstance(list_, List):
-      return RTResult().failure(RTError(
-        self.pos_start, self.pos_end,
-        "First argument must be list",
-        exec_ctx
-      ))
-
-    if not isinstance(index, Number):
-      return RTResult().failure(RTError(
-        self.pos_start, self.pos_end,
-        "Second argument must be number",
-        exec_ctx
-      ))
-
-    try:
-      element = list_.elements.pop(index.value)
-    except:
-      return RTResult().failure(RTError(
-        self.pos_start, self.pos_end,
-        'Element at this index could not be removed from list because index is out of bounds',
-        exec_ctx
-      ))
-    return RTResult().success(element)
-  execute_pop.arg_names = ["list", "index"]
-
-  def execute_extend(self, exec_ctx):
-    listA = exec_ctx.symbol_table.get("listA")
-    listB = exec_ctx.symbol_table.get("listB")
-
-    if not isinstance(listA, List):
-      return RTResult().failure(RTError(
-        self.pos_start, self.pos_end,
-        "First argument must be list",
-        exec_ctx
-      ))
-
-    if not isinstance(listB, List):
-      return RTResult().failure(RTError(
-        self.pos_start, self.pos_end,
-        "Second argument must be list",
-        exec_ctx
-      ))
-
-    listA.elements.extend(listB.elements)
-    return RTResult().success(Number.null)
-  execute_extend.arg_names = ["listA", "listB"]
-
   def execute_abs(self, exec_ctx):
     num = exec_ctx.symbol_table.get("value")
 
@@ -645,6 +554,15 @@ class BuiltInFunction(BaseFunction):
         exec_ctx
       ))
       
+    try:
+      math.log10(num.to_realnum())
+    except ValueError:
+      return RTResult().failure(RTError(
+        self.pos_start,self.pos_end,
+        "LOG : Out of domain.",
+        exec_ctx
+      ))
+
     if num.to_realnum() == 0:
       return RTResult().success(Number(- math.inf))
     elif math.log10(num.to_realnum()) % 1:
