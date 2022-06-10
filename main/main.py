502d0187238c176e9ae7be75576efcad0904c1e1
Erfan Hüseyini <56768093+erfgram@users.noreply.github.com>
2022-06-10 18:24:43 +0430

    Unused codes removed

--- main/main.py
+++ main/main.py
@@ -17,34 +17,15 @@ class RTResult:
     self.value = None
     self.error = None
     self.func_return_value = None
-    self.loop_should_continue = False
-    self.loop_should_break = False
 
   def register(self, res):
     self.error = res.error
     self.func_return_value = res.func_return_value
-    self.loop_should_continue = res.loop_should_continue
-    self.loop_should_break = res.loop_should_break
     return res.value
 
   def success(self, value):
     self.reset()
     self.value = value
-    return self
-
-  def success_return(self, value):
-    self.reset()
-    self.func_return_value = value
-    return self
-  
-  def success_continue(self):
-    self.reset()
-    self.loop_should_continue = True
-    return self
-
-  def success_break(self):
-    self.reset()
-    self.loop_should_break = True
     return self
 
   def failure(self, error):
@@ -56,9 +37,7 @@ class RTResult:
     # Note: this will allow you to continue and break outside the current function
     return (
       self.error or
-      self.func_return_value or
-      self.loop_should_continue or
-      self.loop_should_break
+      self.func_return_value
     )
 
 # VALUES
@@ -98,27 +77,6 @@ class Value:
     return None, self.illegal_operation(other)
 
   def get_comparison_ne(self, other):
-    return None, self.illegal_operation(other)
-
-  def get_comparison_lt(self, other):
-    return None, self.illegal_operation(other)
-
-  def get_comparison_gt(self, other):
-    return None, self.illegal_operation(other)
-
-  def get_comparison_lte(self, other):
-    return None, self.illegal_operation(other)
-
-  def get_comparison_gte(self, other):
-    return None, self.illegal_operation(other)
-
-  def anded_by(self, other):
-    return None, self.illegal_operation(other)
-
-  def ored_by(self, other):
-    return None, self.illegal_operation(other)
-
-  def notted(self, other):
     return None, self.illegal_operation(other)
 
   def execute(self, args):
@@ -208,45 +166,6 @@ class Number(Value):
       return Number(int(self.value != other.value)).set_context(self.context), None
     else:
       return None, Value.illegal_operation(self, other)
-
-  def get_comparison_lt(self, other):
-    if isinstance(other, Number):
-      return Number(int(self.value < other.value)).set_context(self.context), None
-    else:
-      return None, Value.illegal_operation(self, other)
-
-  def get_comparison_gt(self, other):
-    if isinstance(other, Number):
-      return Number(int(self.value > other.value)).set_context(self.context), None
-    else:
-      return None, Value.illegal_operation(self, other)
-
-  def get_comparison_lte(self, other):
-    if isinstance(other, Number):
-      return Number(int(self.value <= other.value)).set_context(self.context), None
-    else:
-      return None, Value.illegal_operation(self, other)
-
-  def get_comparison_gte(self, other):
-    if isinstance(other, Number):
-      return Number(int(self.value >= other.value)).set_context(self.context), None
-    else:
-      return None, Value.illegal_operation(self, other)
-
-  def anded_by(self, other):
-    if isinstance(other, Number):
-      return Number(int(self.value and other.value)).set_context(self.context), None
-    else:
-      return None, Value.illegal_operation(self, other)
-
-  def ored_by(self, other):
-    if isinstance(other, Number):
-      return Number(int(self.value or other.value)).set_context(self.context), None
-    else:
-      return None, Value.illegal_operation(self, other)
-
-  def notted(self):
-    return Number(1 if self.value == 0 else 0).set_context(self.context), None
 
   def copy(self):
     copy = Number(self.value)
